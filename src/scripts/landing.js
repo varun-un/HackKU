ca4464357550c572dbd8181da95ee86c67a790a8
varun_u <77634835+astroameba@users.noreply.github.com>
2021-04-10 09:55:52 -0400

    set up connections + cluster

---
+++ src/scripts/landing.js
@@ -0,0 +1,14 @@
+var MongoClient = import('mongodb');
+MongoClient = MongoClient.MongoClient;
+const uri = "mongodb+srv://general_access:jjzrqmvtwbh1TNcu@nutritious.llyyo.mongodb.net/nutritioUS?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+var email = "";
+client.connect(err => {
+    const collection = client.db("nutritioUS").collection("user_accounts");
+    // perform actions on the collection object
+    collection.find({email: 'carl.wheezer321@gmail.com'}).toArray((error, items) => {
+        console.log(items)
+    });
+
+    client.close();
+});
